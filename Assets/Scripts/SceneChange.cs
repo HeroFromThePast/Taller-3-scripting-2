170732ec375b6e7dee3cf5040c53747f0ec241f0
Juan José Pérez Sánchez <60188878+HeroFromThePast@users.noreply.github.com>
2022-05-06 11:54:27 -0500

    a
    a

--- Assets/Scripts/SceneChange.cs
+++ Assets/Scripts/SceneChange.cs
@@ -1,18 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneChange : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void GoMain()
     {
-        
+        SceneManager.LoadScene(0);
     }
-
-    // Update is called once per frame
-    void Update()
+    public void GoPunto1()
     {
-        
+        SceneManager.LoadScene(1);
     }
 }
